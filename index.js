ea2e698fd2262a8bc4ac46702f17bdf894f1cf13
JoaoLucasKnight <kirajota1@gmail.com>
2023-01-06 19:02:33 -0300

    primeira aula de node

--- index.js
+++ index.js
@@ -1,1 +1,51 @@
-console.log('olá mundo');
+import fs from 'fs';
+import chalk from 'chalk';
+
+
+
+
+function trataErro (erro) {
+        throw new Error(chalk.red(erro.code, 'Fez merda jumento'));
+   }
+// ultilizando o Async e o await 
+
+async function pegaArq (caminho){
+    try { 
+    const encoding = 'utf-8'; 
+
+    const texto = await fs.promises.readFile(caminho,encoding);
+    console.log(chalk.green(texto))
+    } catch(erro){
+        trataErro(erro)
+    }
+}
+// alem do try (caminho feliz) catch ( o caminho do erro ) temos tambem o finally que sempre sera executado indepedente do caso 
+
+
+
+// Ultilizando o Then()
+/* function pegaArq (caminho){
+     const encoding = 'utf-8'; 
+    
+        fs.promises
+        .readFile(caminho, encoding)
+        .then((texto) => console.log(chalk.green(texto)))
+        .catch((erro) => trataErro(erro)) // tambem pode ser feito dessa formas ===  .catch(trataErro)
+    } */
+
+
+
+pegaArq('./arquivos/texto.md');
+
+
+/* function pegaArquivo (caminho){ sicrona 
+    const encoding = 'utf-8';
+    
+    fs.readFile(caminho, encoding, (erro,texto)=>{
+         if(erro){
+            trataErro(chalk.red(erro.code, 'Fez merda jumento'));
+         } else{
+            console.log(chalk.green(texto))
+         }   
+    });
+}; */
