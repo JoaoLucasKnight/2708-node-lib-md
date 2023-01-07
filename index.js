5a0c3e84339ad2343d36bdb88f4581fbeed38d51
JoaoLucasKnight <kirajota1@gmail.com>
2023-01-07 12:12:42 -0300

    estudo do sabado

--- index.js
+++ index.js
@@ -2,11 +2,19 @@ import fs from 'fs';
 import chalk from 'chalk';
 
 
+const testeTexto = 'São geralmente recuperados a partir de um objeto [FileList](https://developer.mozilla.org/pt-BR/docs/Web/API/FileList) que é retornado como resultado da seleção, pelo usuário, de arquivos através do elemento [<input>](https://developer.mozilla.org/pt-BR/docs/Web/HTML/Element/Input), a partir do objeto [DataTransfer](https://developer.mozilla.org/pt-BR/docs/Web/API/DataTransfer) utilizado em operações de arrastar e soltar, ou a partir da API `mozGetAsFile()` em um [HTMLCanvasElement](https://developer.mozilla.org/pt-BR/docs/Web/API/HTMLCanvasElement). Em Gecko, códigos com privilégiios podem criar objetos File representando qualquer arquivo local sem a intereção do usuário (veja [Implementation notes](https://developer.mozilla.org/pt-BR/docs/Web/API/File#implementation_notes) para mais informações.).'
+
+function tiraLink (texto){
+    const regex = /\[([^[\]]*?)\]\((https?[^\s]*)\)/gm
+    const capturas = texto.match(regex);
+    console.log(capturas)
+
+}
+
+tiraLink(testeTexto);
 
 
-function trataErro (erro) {
-        throw new Error(chalk.red(erro.code, 'Fez merda jumento'));
-   }
+
 // ultilizando o Async e o await 
 
 async function pegaArq (caminho){
@@ -19,7 +27,18 @@ async function pegaArq (caminho){
         trataErro(erro)
     }
 }
+// ---------------- Comentarios --------------
+
 // alem do try (caminho feliz) catch ( o caminho do erro ) temos tambem o finally que sempre sera executado indepedente do caso 
+
+// \[[^[\]]*?\]
+//\(https?[^\s]*\)
+// \[([^[\]]*?)\]\((https?[^\s]*)\)
+
+
+
+
+
 
 
 
@@ -32,11 +51,6 @@ async function pegaArq (caminho){
         .then((texto) => console.log(chalk.green(texto)))
         .catch((erro) => trataErro(erro)) // tambem pode ser feito dessa formas ===  .catch(trataErro)
     } */
-
-
-
-pegaArq('./arquivos/texto.md');
-
 
 /* function pegaArquivo (caminho){ sicrona 
     const encoding = 'utf-8';
